4d4c607c493963d623ca37f501f9edea97071e3a
StackDoubleFlow <ojaslandge@gmail.com>
2021-09-19 16:42:41 -0500

    Start icalls codegen

---
+++ src/Codegen/ClassInlines.cpp
@@ -0,0 +1,18 @@
+#include "beatsaber-hook/shared/utils/utils.h"
+#include "codegen/il2cpp-codegen.h"
+
+namespace il2cpp::vm {
+
+bool ClassInlines::InitFromCodegenSlow(Il2CppClass *klass) { SAFE_ABORT(); }
+
+const VirtualInvokeData &ClassInlines::GetInterfaceInvokeDataFromVTableSlowPath(
+    Il2CppObject *obj, const Il2CppClass *itf, Il2CppMethodSlot slot) {
+    SAFE_ABORT();
+}
+
+const VirtualInvokeData *ClassInlines::GetInterfaceInvokeDataFromVTableSlowPath(
+    const Il2CppClass *klass, const Il2CppClass *itf, Il2CppMethodSlot slot) {
+    SAFE_ABORT();
+}
+
+} // namespace il2cpp::vm
---
+++ src/Codegen/CodegenAPI.cpp
@@ -0,0 +1,227 @@
+#include "codegen/il2cpp-codegen.h"
+#include "beatsaber-hook/shared/utils/utils.h"
+
+// TODO: Log unimplemented
+#define UNIMPLEMENTED(str) SAFE_ABORT()
+
+void il2cpp_codegen_marshal_store_last_error() { UNIMPLEMENTED(); }
+
+Il2CppAsyncResult* il2cpp_codegen_delegate_begin_invoke(RuntimeDelegate* delegate, void** params, RuntimeDelegate* asyncCallback, RuntimeObject* state) { UNIMPLEMENTED(); }
+
+RuntimeObject* il2cpp_codegen_delegate_end_invoke(Il2CppAsyncResult* asyncResult, void **out_args) { UNIMPLEMENTED(); }
+
+Il2CppMethodPointer il2cpp_codegen_resolve_icall(const char* name) { UNIMPLEMENTED(); }
+
+Type_t* il2cpp_codegen_type_get_object(const RuntimeType* type) { UNIMPLEMENTED(); }
+
+MethodBase_t* il2cpp_codegen_get_method_object_internal(const RuntimeMethod* method, RuntimeClass* refclass) { UNIMPLEMENTED(); }
+
+Assembly_t* il2cpp_codegen_get_executing_assembly(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_register(const Il2CppCodeRegistration* const codeRegistration, const Il2CppMetadataRegistration* const metadataRegistration, const Il2CppCodeGenOptions* const codeGenOptions) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_initialize_method(uint32_t index) { UNIMPLEMENTED(); }
+
+const RuntimeMethod* il2cpp_codegen_get_generic_method_definition(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+void* il2cpp_codegen_get_thread_static_data(RuntimeClass* klass) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_memory_barrier() { UNIMPLEMENTED(); }
+
+RuntimeArray* SZArrayNew(RuntimeClass* arrayType, uint32_t length) { UNIMPLEMENTED(); }
+
+RuntimeArray* GenArrayNew(RuntimeClass* arrayType, il2cpp_array_size_t* dimensions) { UNIMPLEMENTED(); }
+
+bool il2cpp_codegen_method_is_generic_instance(RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+RuntimeClass* il2cpp_codegen_method_get_declaring_type(RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+bool MethodIsStatic(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+bool MethodHasParameters(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+NORETURN void il2cpp_codegen_raise_profile_exception(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+const RuntimeMethod* il2cpp_codegen_get_generic_virtual_method_internal(const RuntimeMethod* methodDefinition, const RuntimeMethod* inflatedMethod) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_runtime_class_init(RuntimeClass* klass) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_raise_execution_engine_exception(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_raise_execution_engine_exception_if_method_is_not_found(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+RuntimeObject* IsInst(RuntimeObject *obj, RuntimeClass* targetType) { UNIMPLEMENTED(); }
+
+RuntimeObject* Box(RuntimeClass* type, void* data) { UNIMPLEMENTED(); }
+
+void* Unbox_internal(Il2CppObject* obj) { UNIMPLEMENTED(); }
+
+void UnBoxNullable_internal(RuntimeObject* obj, RuntimeClass* expectedBoxedClass, void* storage) { UNIMPLEMENTED(); }
+
+RuntimeObject* il2cpp_codegen_object_new(RuntimeClass *klass) { UNIMPLEMENTED(); }
+
+void* il2cpp_codegen_marshal_allocate(size_t size) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_profiler_method_enter(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_profiler_method_exit(const RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+NORETURN void il2cpp_codegen_raise_exception(Exception_t *ex, MethodInfo* lastManagedFrame) { UNIMPLEMENTED(); }
+
+NORETURN void il2cpp_codegen_raise_exception(il2cpp_hresult_t hresult, bool defaultToCOMException) { UNIMPLEMENTED(); }
+
+NORETURN void il2cpp_codegen_raise_out_of_memory_exception() { UNIMPLEMENTED(); }
+
+NORETURN void il2cpp_codegen_raise_null_reference_exception() { UNIMPLEMENTED(); }
+
+NORETURN void il2cpp_codegen_raise_divide_by_zero_exception() { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_argument_exception(const char* param, const char* msg) { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_argument_null_exception(const char* param) { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_overflow_exception() { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_not_supported_exception(const char* msg) { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_array_type_mismatch_exception() { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_invalid_cast_exception(const char* msg) { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_invalid_operation_exception(const char* msg) { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_marshal_directive_exception(const char* msg) { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_missing_method_exception(const char* msg) { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_maximum_nested_generics_exception() { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_index_out_of_range_exception() { UNIMPLEMENTED(); }
+
+Exception_t* il2cpp_codegen_get_exception(il2cpp_hresult_t hresult, bool defaultToCOMException) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_store_exception_info(RuntimeException* ex, String_t* exceptionString) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_com_marshal_variant(RuntimeObject* obj, Il2CppVariant* variant) { UNIMPLEMENTED(); }
+
+RuntimeObject* il2cpp_codegen_com_marshal_variant_result(const Il2CppVariant* variant) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_com_destroy_variant(Il2CppVariant* variant) { UNIMPLEMENTED(); }
+
+Il2CppSafeArray* il2cpp_codegen_com_marshal_safe_array(Il2CppChar type, RuntimeArray* managedArray) { UNIMPLEMENTED(); }
+
+RuntimeArray* il2cpp_codegen_com_marshal_safe_array_result(Il2CppChar variantType, RuntimeClass* type, Il2CppSafeArray* safeArray) { UNIMPLEMENTED(); }
+
+Il2CppSafeArray* il2cpp_codegen_com_marshal_safe_array_bstring(RuntimeArray* managedArray) { UNIMPLEMENTED(); }
+
+RuntimeArray* il2cpp_codegen_com_marshal_safe_array_bstring_result(RuntimeClass* type, Il2CppSafeArray* safeArray) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_com_destroy_safe_array(Il2CppSafeArray* safeArray) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_com_create_instance(const Il2CppGuid& clsid, Il2CppIUnknown** identity) { UNIMPLEMENTED(); }
+
+il2cpp_hresult_t il2cpp_codegen_com_handle_invalid_iproperty_conversion(const char* fromType, const char* toType) { UNIMPLEMENTED(); }
+
+il2cpp_hresult_t il2cpp_codegen_com_handle_invalid_iproperty_conversion(RuntimeObject* value, const char* fromType, const char* toType) { UNIMPLEMENTED(); }
+
+il2cpp_hresult_t il2cpp_codegen_com_handle_invalid_ipropertyarray_conversion(const char* fromArrayType, const char* fromElementType, const char* toElementType, il2cpp_array_size_t index) { UNIMPLEMENTED(); }
+
+il2cpp_hresult_t il2cpp_codegen_com_handle_invalid_ipropertyarray_conversion(RuntimeObject* value, const char* fromArrayType, const char* fromElementType, const char* toElementType, il2cpp_array_size_t index) { UNIMPLEMENTED(); }
+
+Il2CppIUnknown* il2cpp_codegen_com_get_or_create_ccw_internal(RuntimeObject* obj, const Il2CppGuid& iid) { UNIMPLEMENTED(); }
+
+Il2CppObject* il2cpp_codegen_com_unpack_ccw(Il2CppIUnknown* obj) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_com_register_rcw(Il2CppComObject* rcw) { UNIMPLEMENTED(); }
+
+RuntimeObject* il2cpp_codegen_com_get_or_create_rcw_from_iunknown_internal(Il2CppIUnknown* unknown, RuntimeClass* fallbackClass) { UNIMPLEMENTED(); }
+
+RuntimeObject* il2cpp_codegen_com_get_or_create_rcw_from_iinspectable_internal(Il2CppIInspectable* unknown, RuntimeClass* fallbackClass) { UNIMPLEMENTED(); }
+
+RuntimeObject* il2cpp_codegen_com_get_or_create_rcw_for_sealed_class_internal(Il2CppIUnknown* unknown, RuntimeClass* objectClass) { UNIMPLEMENTED(); }
+
+Il2CppIUnknown* il2cpp_codegen_com_query_interface_internal(Il2CppComObject* rcw, const Il2CppGuid& guid) { UNIMPLEMENTED(); }
+
+Il2CppIUnknown* il2cpp_codegen_com_query_interface_no_throw_internal(Il2CppComObject* rcw, const Il2CppGuid& guid) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_com_cache_queried_interface(Il2CppComObject* rcw, const Il2CppGuid& iid, Il2CppIUnknown* queriedInterface) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_il2cpp_com_object_cleanup(Il2CppComObject* rcw) { UNIMPLEMENTED(); }
+
+String_t* il2cpp_codegen_string_new_wrapper(const char* str) { UNIMPLEMENTED(); }
+
+String_t* il2cpp_codegen_string_new_utf16(const il2cpp::utils::StringView<Il2CppChar>& str) { UNIMPLEMENTED(); }
+
+RuntimeString* il2cpp_codegen_type_append_assembly_name_if_necessary(RuntimeString* typeName, const char* assemblyName) { UNIMPLEMENTED(); }
+
+NORETURN void RaiseInvalidCastException(RuntimeObject* obj, RuntimeClass* targetType) { UNIMPLEMENTED(); }
+
+bool il2cpp_codegen_method_is_interface_method(RuntimeMethod* method) { UNIMPLEMENTED(); }
+
+bool il2cpp_codegen_class_is_assignable_from(RuntimeClass *klass, RuntimeClass *oklass) { UNIMPLEMENTED(); }
+
+bool il2cpp_codegen_class_is_value_type(RuntimeClass* type) { UNIMPLEMENTED(); }
+
+RuntimeClass* il2cpp_codegen_inflate_generic_class(RuntimeClass* genericClassDefinition, const Il2CppGenericInst* genericInst) { UNIMPLEMENTED(); }
+
+int32_t il2cpp_codgen_class_get_instance_size(RuntimeClass* klass) { UNIMPLEMENTED(); }
+
+RuntimeClass* il2cpp_codegen_class_from_type_internal(const RuntimeType* type) { UNIMPLEMENTED(); }
+
+char* il2cpp_codegen_marshal_string(String_t* string) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_marshal_string_fixed(String_t* string, char* buffer, int numberOfCharacters) { UNIMPLEMENTED(); }
+
+Il2CppChar* il2cpp_codegen_marshal_wstring(String_t* string) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_marshal_wstring_fixed(String_t* string, Il2CppChar* buffer, int numberOfCharacters) { UNIMPLEMENTED(); }
+
+Il2CppChar* il2cpp_codegen_marshal_bstring(String_t* string) { UNIMPLEMENTED(); }
+
+String_t* il2cpp_codegen_marshal_string_result(const char* value) { UNIMPLEMENTED(); }
+
+String_t* il2cpp_codegen_marshal_wstring_result(const Il2CppChar* value) { UNIMPLEMENTED(); }
+
+String_t* il2cpp_codegen_marshal_bstring_result(const Il2CppChar* value) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_marshal_free_bstring(Il2CppChar* value) { UNIMPLEMENTED(); }
+
+char* il2cpp_codegen_marshal_empty_string_builder(StringBuilder_t* stringBuilder) { UNIMPLEMENTED(); }
+
+char* il2cpp_codegen_marshal_string_builder(StringBuilder_t* stringBuilder) { UNIMPLEMENTED(); }
+
+Il2CppChar* il2cpp_codegen_marshal_empty_wstring_builder(StringBuilder_t* stringBuilder) { UNIMPLEMENTED(); }
+
+Il2CppChar* il2cpp_codegen_marshal_wstring_builder(StringBuilder_t* stringBuilder) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_marshal_string_builder_result(StringBuilder_t* stringBuilder, char* buffer) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_marshal_wstring_builder_result(StringBuilder_t* stringBuilder, Il2CppChar* buffer) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_marshal_free(void* ptr) { UNIMPLEMENTED(); }
+
+Il2CppMethodPointer il2cpp_codegen_marshal_delegate(MulticastDelegate_t* d) { UNIMPLEMENTED(); }
+
+Il2CppDelegate* il2cpp_codegen_marshal_function_ptr_to_delegate_internal(void* functionPtr, Il2CppClass* delegateType) { UNIMPLEMENTED(); }
+
+Il2CppMethodPointer il2cpp_codegen_resolve(const PInvokeArguments& pinvokeArgs) { UNIMPLEMENTED(); }
+
+Il2CppHString il2cpp_codegen_create_hstring(String_t* str) { UNIMPLEMENTED(); }
+
+String_t* il2cpp_codegen_marshal_hstring_result(Il2CppHString hstring) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_marshal_free_hstring(Il2CppHString hstring) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_marshal_type_to_native(Type_t* type, Il2CppWindowsRuntimeTypeName& nativeType) { UNIMPLEMENTED(); }
+
+const Il2CppType* il2cpp_codegen_marshal_type_from_native_internal(Il2CppWindowsRuntimeTypeName& nativeType) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_delete_native_type(Il2CppWindowsRuntimeTypeName& nativeType) { UNIMPLEMENTED(); }
+
+Il2CppIActivationFactory* il2cpp_codegen_windows_runtime_get_activation_factory(const il2cpp::utils::StringView<Il2CppNativeChar>& runtimeClassName) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_stacktrace_push_frame(Il2CppStackFrameInfo& frame) { UNIMPLEMENTED(); }
+
+void il2cpp_codegen_stacktrace_pop_frame() { UNIMPLEMENTED(); }
+
+const char* il2cpp_codegen_get_field_data(RuntimeField* field) { UNIMPLEMENTED(); }
