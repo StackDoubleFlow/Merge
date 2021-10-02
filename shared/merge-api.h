52db7e479fb4b35c83207a24d211d7541f6d89cd
StackDoubleFlow <ojaslandge@gmail.com>
2021-10-01 22:56:58 -0500

    Grammar

--- shared/merge-api.h
+++ shared/merge-api.h
@@ -236,8 +236,8 @@ void SetCustomAttributes(ImageIndex image,
  * Offsets the size of a type definition. This can be useful for custom c++
  * fields or anything extra that il2cpp cannot calculate itself.
  *
- * It can also subtract the size but that sounds like it can go horribly
- * wrongly.
+ * It can also subtract the size, but that sounds like it can go horribly
+ * wrong so I wouldn't recommend it.
  *
  * @param type The type defintion to modify.
  * @param sizeOffset The amount of bytes to offset the size by.
